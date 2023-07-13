ac568aaeb0047586deb402f81e0866e5f81d2e88
Dan <sidato83@gmail.com>
2023-07-12 19:09:56 -0700

    Human class with constructors and intro method

---
+++ Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CreatingClasses
+{
+    internal class Human
+    {
+        private string firstName = "FName";
+        private string lastName = "LName";
+        private string eyeColor = "Color";
+        private int age = 0;
+
+        // Default constructor (no values).
+        public Human()
+        {
+            Console.WriteLine("Constructor called. Default Human object");
+        }
+
+        // Default constructor (optional values).
+        public Human(string firstName = "FName", string lastName = "LName",
+            string eyeColor = "Color", int age = 0)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void Introduction()
+        {
+            string plural = CheckAge();
+
+            Console.WriteLine($"Hi, my name is {firstName} {lastName}. I am {age} {plural} old, " +
+                $"and I have {eyeColor} eyes.");
+        }
+
+        private string CheckAge() => (age == 1) ? "year" : "years";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CreatingClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human bot = new Human();
+            Human tim = new Human("Timmy", "Hans", "Blue", 34);
+
+            bot.Introduction();
+            tim.Introduction();
+
+            Console.ReadKey();
+        }
+    }
+}
